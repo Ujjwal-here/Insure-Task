5d4ca37ec17d412c1e83b976097d1148bafa1908
Ujjwal-here <ujjwalsbx@gmail.com>
2022-01-16 21:14:56 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import {useEffect,useState} from "react"
-import axios from "axios"
-import Posts from "./components/Posts";
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+import axios from 'axios';
+import './App.css';
+import Search from './components/Search';
 
-function App() {
-  const [post,setPost] =useState([])
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
-     const fetch= async()=>{
-        const data= await axios.get("https://jsonplaceholder.typicode.com/posts")
-        setPost(data.data)
-      }
-     fetch()
-  }, [])
+    const fetchPosts = async () => {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
-    <Posts p={post}/>
+    <div className='container mt-5'>
+      <Search/>
+      <Posts posts={currentPosts} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,25 +1,18 @@
-import React from "react";
-import "./Post.css";
+import React from 'react';
 
-const Posts = ({ p }) => {
-  console.log(p);
+const Posts = ({ posts, loading }) => {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <>
-      <div class="container">
-        {p.map((post) => (
-          <div key={post.id} class="card">
-            <div class="circle">
-              <h2>{post.userId}</h2>
-            </div>
-            
-            <div class="content">
-            <h4>{post.title}</h4>
-              <p>{post.body}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
+    <ul className='list-group mb-4'>
+      {posts.map(post => (
+        <li key={post.id} className='list-group-item'>
+          {post.title}
+        </li>
+      ))}
+    </ul>
   );
 };
 
